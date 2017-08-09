ad61f1137972ef90ae77084d64c05646073da493
Dave Hards <unstablesun@gmail.com>
2017-08-08 20:59:07 -0700

    Tiled assets

---
+++ RawAssets/Tiled/TileSets/Platforms.tsx
@@ -0,0 +1,25 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="BlockBacks" tilewidth="1128" tileheight="1287" tilecount="4" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image width="1128" height="1287" source="Block2.png"/>
+  <objectgroup draworder="index">
+   <object id="6" x="14" y="7" width="348" height="384"/>
+  </objectgroup>
+ </tile>
+ <tile id="1">
+  <image width="229" height="296" source="Block3.png"/>
+  <objectgroup draworder="index">
+   <object id="1" x="14" y="15" width="332" height="380"/>
+  </objectgroup>
+ </tile>
+ <tile id="2">
+  <image width="313" height="290" source="Block1.png"/>
+  <objectgroup draworder="index">
+   <object id="1" x="1" y="-1" width="313" height="290"/>
+  </objectgroup>
+ </tile>
+ <tile id="3">
+  <image width="32" height="32" source="Block4.png"/>
+ </tile>
+</tileset>
---
+++ RawAssets/Tiled/TileSets/platformPieces2.tsx
@@ -0,0 +1,30 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="PlatformPieces2" tilewidth="64" tileheight="64" tilecount="6" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image width="64" height="64" source="BlockGrassPlatform1.png"/>
+  <objectgroup draworder="index">
+   <object id="1" x="0" y="-0.25" width="64" height="64"/>
+  </objectgroup>
+ </tile>
+ <tile id="1">
+  <image width="64" height="64" source="BlockRocks1.png"/>
+ </tile>
+ <tile id="2">
+  <image width="64" height="64" source="BlockRocks2.png"/>
+ </tile>
+ <tile id="3">
+  <image width="64" height="64" source="BlockRocks3.png"/>
+ </tile>
+ <tile id="4">
+  <image width="64" height="64" source="BlockGrassPlatform45.png"/>
+  <objectgroup draworder="index">
+   <object id="1" x="0.25" y="59">
+    <polygon points="0,0 63.25,-59.25 63.75,4.25 0,5"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="5">
+  <image width="64" height="64" source="Tree1.png"/>
+ </tile>
+</tileset>
